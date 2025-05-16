70020d901d4a5463a23655b213f50c64dbe6e496
varunriyer <vs7835@srmist.edu.in>
2025-05-16 17:33:06 +0530

    Fix HTTP method for forgot-password route from GET to POST

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/profile', protect, getUserProfile);
-router.get('/forgot-password', forgotPassword);
+router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:resetToken', resetPassword);
 
 export default router;
