7a2b64ffec9447fa437c1ab67824344af5af44d8
varunriyer <vs7835@srmist.edu.in>
2025-05-16 16:22:58 +0530

    Add authentication routes for user signup, login, and password management

---
+++ routes/authRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+    signup,
+    login,
+    getUserProfile,
+    forgotPassword,
+    resetPassword,
+} from '../controllers/authControllers.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router();
+
+//Authentication Routes
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/profile', protect, getUserProfile);
+router.get('/forgot-password', forgotPassword);
+router.post('/reset-password/:resetToken', resetPassword);
+
+export default router;
