a04bbfa5d9c2219c122f2e579a87229050e8a5e3
varunriyer <vs7835@srmist.edu.in>
2025-05-16 16:37:09 +0530

    Fix case sensitivity issue in import.meta.url for Swagger JSON file loading

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 //Load Swagger JSON file 
 const swaggerFile = JSON.parse(
-    await readFile(new URL('./swagger.json', import.meta.URL))
+    await readFile(new URL('./swagger.json', import.meta.url))
 );
 
 // Swagger Documentation
