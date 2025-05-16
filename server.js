86b85f8524924e73d3e6f926af8b209f6b87eab8
varunriyer <vs7835@srmist.edu.in>
2025-05-16 12:17:50 +0530

    Add server.js file to set up Express server with MongoDB connection and Swagger documentation

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { readFile } from 'fs/promises'
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import noteRoutes from './routes/noteRoutes.js';
+
+//To load environment variables
+dotenv.config();
+
+//To connect to MongoDB Database
+connectDB();
+
+const app = express();
+
+//Middleware 
+app.use(cors());
+app.use(express.json());
+
+//Load Swagger JSON file 
+const swaggerFile = JSON.parse(
+    await readFile(new URL('./swagger.json', import.meta.URL))
+);
+
+// Swagger Documentation
+app.use('./api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+//Routes 
+
+app.use('/api/auth', authRoutes);
+app.use('/api/notes', noteRoutes);
+
+//Home Route
+app.use('/', (req, res) => {
+    res.json({ message: 'Welcome to My Notes API' });
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ message: 'Server Error', error: err.message });
+});
+
+//Start Server
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+
