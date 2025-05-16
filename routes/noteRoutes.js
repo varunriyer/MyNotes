1bde5aa0329c7a8f50965719fe6ba215761dabe5
varunriyer <vs7835@srmist.edu.in>
2025-05-16 16:27:55 +0530

    Add note routes for CRUD operations and protect middleware

---
+++ routes/noteRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import {
+    getNotes,
+    getNoteByID,
+    createNote,
+    updateNote,
+    deleteNote,
+} from '../controllers/noteController.js';
+import { protect } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.use(protect);
+
+//Note Operations Routes
+
+router.route('/')
+    .get(getNotes)
+    .post(createNote);
+
+router.route('/:id')
+    .get(getNoteByID)
+    .put(updateNote)
+    .delete(deleteNote);
+
+export default router;
