9f31486b40e159c80e920071a9302620abade5e2
varunriyer <vs7835@srmist.edu.in>
2025-05-16 14:08:15 +0530

    Add noteModel.js to define schema for notes with title, content, and user references

---
+++ models/noteModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required'],
+            trim: true,
+        },
+        content: {
+            type: String,
+            required: [true, 'Content is required'],
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const Note = mongoose.model('Note', noteSchema);
+export default Note;
