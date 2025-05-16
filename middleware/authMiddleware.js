d6d8250173838dda27e4ca83f3af9dac32e21e1c
varunriyer <vs7835@srmist.edu.in>
2025-05-16 16:33:14 +0530

    Fix import path for User model in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/authModel.js';
 
 // Protect routes middleware
 export const protect = async (req, res, next) => {
